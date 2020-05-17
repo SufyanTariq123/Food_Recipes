7ec86fa1616b6f6b4106cec279e56bba96a8fc0f
SufyanTariq123 <SufyanTariq056@gmail.com>
2020-05-17 18:04:34 +0500

    Updates

---
+++ precache-manifest.544801eaca4f9cf4692f0af4a8f81354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7d417abb2943daab8cadea02a1fd96",
+    "url": "/Food_Recipes/index.html"
+  },
+  {
+    "revision": "e0572b3423aa1364fc30",
+    "url": "/Food_Recipes/static/css/main.bee3e378.chunk.css"
+  },
+  {
+    "revision": "3af64690e32988179fbb",
+    "url": "/Food_Recipes/static/js/2.1ac56555.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Food_Recipes/static/js/2.1ac56555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0572b3423aa1364fc30",
+    "url": "/Food_Recipes/static/js/main.18bdaaad.chunk.js"
+  },
+  {
+    "revision": "27979314aa696c844186",
+    "url": "/Food_Recipes/static/js/runtime-main.34b4ec42.js"
+  },
+  {
+    "revision": "20ed7125273de374a44e2dc61a769637",
+    "url": "/Food_Recipes/static/media/Food.20ed7125.png"
+  }
+]);
